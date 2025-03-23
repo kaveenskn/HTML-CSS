aef96777c98cf7b382db47f4da9e80c6269c1c0f
kaveenskn <shanmugarajakaveen4@gmail.com>
2025-03-23 19:52:23 +0530

    New files added

---
+++ MoonEffect/theme.js
@@ -0,0 +1,10 @@
+
+
+icon=document.getElementById("img").onclick=function(){document.body.classList.toggle("dark")
+
+if(document.body.classList.contains("dark")
+){
+img. src=("sun.jpg");}
+else{img.  src=("moon.jpg");}
+
+}
